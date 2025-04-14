5687cc9f640a068421765cc01fe6c72b03b6cb40
Gennadiy <shindyapingena@gmail.com>
2025-04-14 16:03:19 +0400

    small fix

---
+++ src/test/java/service/BookingServiceTest.java
@@ -0,0 +1,43 @@
+package service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BookingServiceTest {
+
+    @BeforeEach
+    void setUp() {
+        //TODO: По необходимости
+    }
+
+    @Test
+    @DisplayName("Проверка метода isRoomAvailable")
+    void givenRoomAndCheckInDate_whenCheckAvailable_thenReturnTrue() {
+        //TODO: Тест в формате given when then
+    }
+
+    @Test
+    @DisplayName("Проверка метода isRoomAvailable")
+    void givenRoomAndCheckInDate_whenCheckAvailable_thenReturnFalse() {
+        //TODO: Тест в формате given when then
+    }
+
+    @Test
+    @DisplayName("Проверка функциональности bookRoom")
+    void givenRoomAndCheckDates_whenBook_thenReturnTrue() {
+        //TODO: Тест в формате given when then
+    }
+
+    @Test
+    @DisplayName("Проверка функциональности bookRoom")
+    void givenRoomAndCheckDates_whenBook_thenReturnFalse() {
+        //TODO: Тест в формате given when then
+    }
+
+    @Test
+    @DisplayName("Проверка функциональности calculatePrice")
+    void givenRoomAndCheckDates_whenCalculateSum_thenReturnSum() {
+        //TODO: Тест в формате given when then
+    }
+}
