908446cad15ad3b324a478e61131d0d48d24e7d5
vya4eslav1k <krosh.das@bk.ru>
2025-04-18 14:13:56 +0300

    feat: BookingService

--- src/main/java/service/BookingService.java
+++ src/main/java/service/BookingService.java
@@ -4,25 +4,39 @@ import model.Hotel;
 import model.Room;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookingService {
 
-    /*
-    Добавить поля в случае необходимости
-     */
+    Map<Integer, Map<LocalDate, LocalDate>> allReservations = new HashMap<>();
 
     public boolean isRoomAvailable(Room room, LocalDate checkIn, LocalDate checkOut) {
-        // TODO: Реализовать проверку доступности номера
-        return false;
+        Map<LocalDate, LocalDate> reservations = allReservations.get(room.id());
+        if (reservations == null) {
+            return true;
+        }
+        for (Map.Entry<LocalDate, LocalDate> entry : reservations.entrySet()) {
+            boolean datesAfterReservation = entry.getKey().isAfter(checkIn) && entry.getValue().isAfter(checkOut);
+            boolean datesBeforeReservation = entry.getKey().isBefore(checkIn) && entry.getValue().isBefore(checkOut);
+            if (!(datesAfterReservation || datesBeforeReservation)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean bookRoom(Hotel hotel, Room room, LocalDate checkIn, LocalDate checkOut) {
-        // TODO: Реализовать логику бронирования номера
-        return false;
+        if (!isRoomAvailable(room, checkIn, checkOut)) {
+            return false;
+        }
+        Map<LocalDate, LocalDate> reservations = allReservations.computeIfAbsent(room.id(), k -> new HashMap<>());
+        reservations.put(checkIn, checkOut);
+        return true;
     }
 
     public double calculatePrice(Room room, LocalDate checkIn, LocalDate checkOut) {
-        // TODO: Реализовать расчет стоимости
-        return 0.0;
+        return ChronoUnit.DAYS.between(checkIn, checkOut) * room.pricePerNight();
     }
 }
